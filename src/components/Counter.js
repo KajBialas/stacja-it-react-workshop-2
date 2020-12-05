9dc6ecf9579b23d26b010f2edcbb8c08aca34b5a
Kaj Białas <kaj.bialas@dev-industry.com>
2020-12-05 12:36:26 +0100

    issue 7 solution

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import Button from './Button';
+import TextSection from './TextSection';
+
+const COUNTER_INIT_VALUE = 10;
+
+const COUNTER_TEXTS = {
+  TITLE: 'Counter',
+  BUTTONS: {
+    INCREMENT: '+',
+    DECREMENT: '-',
+    RESET: 'RESET',
+  }
+}
 
 class Counter extends Component {
   state = {
-    counter: 10,
+    counter: COUNTER_INIT_VALUE,
   }
 
   handleIncrement = () => this.setState({counter: this.state.counter + 1});
+  handleDecrement = () => this.setState({counter: this.state.counter -1});
+  handleReset = () => this.setState({counter: COUNTER_INIT_VALUE});
   // handleIncrement = () => this.setState(prevState => ({counter: prevState.counter + 1}));
 
   render() {
     return (
       <div>
-        Counter: {this.state.counter}
-        <button onClick={this.handleIncrement}>+</button>
+        <TextSection title={COUNTER_TEXTS.TITLE} text={this.state.counter} />
+        <Button handleClick={this.handleIncrement} text={COUNTER_TEXTS.BUTTONS.INCREMENT} />
+        <Button handleClick={this.handleDecrement} text={COUNTER_TEXTS.BUTTONS.DECREMENT} />
+        <Button handleClick={this.handleReset} text={COUNTER_TEXTS.BUTTONS.RESET} />
       </div>
     )
   }
