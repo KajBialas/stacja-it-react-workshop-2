00949277a961609b833c1d8110b6f6d403f22e3e
Kaj Białas <kaj.bialas@dev-industry.com>
2020-12-05 11:11:15 +0100

    issue4

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,16 @@ import React from 'react';
 const HEADER_TEXT = 'Aplikacja React';
 
 function Header() {
+  const additionalHeaderText = 'Warsztat StacjaIT'
+
   return (
     <header>
       <h1>
         {HEADER_TEXT}
       </h1>
+      <h2>
+        {additionalHeaderText}
+      </h2>
     </header>
   )
 }
--- src/components/TextSection.js
+++ src/components/TextSection.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 const TEXT_SECTION_TEXT = 'Treść aplikacji';
 
 class TextSection extends Component {
+  textSectionHeader = 'Sekcja tekstowa';
+
   render() {
     return (
       <div>
+        <h3>
+          {this.textSectionHeader}
+        </h3>
         <p>
           { TEXT_SECTION_TEXT }
         </p>
