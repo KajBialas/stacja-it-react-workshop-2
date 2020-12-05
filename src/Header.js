b2c1067688b99c4439e72ba4631c679172695535
Kaj Białas <kaj.bialas@dev-industry.com>
2020-12-05 10:46:09 +0100

    issue2

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+const HEADER_TEXT = 'Aplikacja React';
+
 function Header() {
   return (
     <header>
       <h1>
-        Aplikacja React
+        {HEADER_TEXT}
       </h1>
     </header>
   )
 }
 
 export default Header;
+
