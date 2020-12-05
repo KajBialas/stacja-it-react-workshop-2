0de5f28b9d255da685bcec70c0c327460d377121
Kaj Białas <kaj.bialas@dev-industry.com>
2020-12-05 14:22:11 +0100

    issue8 dynamic data fetch

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 
 class PostDetails extends Component {
   state = {
+    id: 1,
     postDetails: {},
     loading: false,
     error: false,
   }
 
-  componentDidMount() {
+  fetchData = () => {
     this.setState({
       loading: true,
     });
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    fetch(`https://jsonplaceholder.typicode.com/posts/${this.state.id}`)
       .then(response => {
         if (response.status > 400) {
           throw new Error();
@@ -19,12 +20,30 @@ class PostDetails extends Component {
         return response.json()})
       .then(data => setTimeout(() => {
         this.setState({
-          postDetails: data,
-          loading: false,
-        }
-      )},2000))
+            postDetails: data,
+            loading: false,
+          }
+        )},2000))
       .catch(e => this.setState({error: true, loading: false}));
   }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevState.id !== this.state.id) {
+      this.fetchData();
+    }
+  }
+
+
+  renderSwitch = () => (
+    <div>
+      Current id: {this.state.id}
+      <button onClick={() => this.setState({id: this.state.id + 1})}>+</button>
+    </div>
+  )
 
   render() {
     if (this.state.loading) {
@@ -32,10 +51,14 @@ class PostDetails extends Component {
     }
 
     if (this.state.error) {
-      return (<div>Błąd ładowania danych</div>);
+      return (<div>
+        {this.renderSwitch()}
+        Błąd ładowania danych
+      </div>);
     }
     return (
       <div>
+        {this.renderSwitch()}
         <h3>{this.state.postDetails.title}</h3>
         <p>{this.state.postDetails.body}</p>
       </div>
